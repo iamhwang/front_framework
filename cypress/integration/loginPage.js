b93770f8516b6cf7bf90b09f470e1a9d143b5850
iamhwang <iamhwang9@gmail.com>
2021-10-13 17:43:08 +0900

    e2e: login test

--- cypress/integration/loginPage.js
+++ cypress/integration/loginPage.js
@@ -1,20 +1,51 @@
 describe('App Test', () => {
   it('login and logout', () => {
     cy.visit('/')
+      // 회원가입
       .get('#username')
-      .type('iamE2E')
+      .type('e2eTest')
+      .get('#password')
+      .type('password')
+      .get('button')
+      .contains('Create')
+      .click()
+      // 로그인 시도
+      .get('button')
+      .contains('Login')
+      .click()
+      // 로그인 성공
+      .get('input')
+      .should('value', 'e2eTest')
+      .get('h1')
+      .should('contain', 'PROFILE')
+      // 로그아웃
+      .get('#logoutButton')
+      .click()
+      .get('h1')
+      .should('contain', 'K-concept')
+      // 재로그인
+      .get('#username')
+      .type('e2eTest')
       .get('#password')
       .type('password')
       .get('button')
       .contains('Login')
       .click()
-      .get('input')
-      .should('value', 'iamE2E')
-      .get('h1')
-      .should('contain', 'PROFILE')
-      .get('#logoutButton')
+      // 회원탈퇴
+      .get('button')
+      .contains('Delete')
       .click()
-      .get('h1')
-      .should('contain', 'K-concept');
+      // 재로그인 시도
+      .get('#username')
+      .type('e2eTest')
+      .get('#password')
+      .type('password')
+      .get('button')
+      .contains('Login')
+      .click()
+      // 실패
+      .get('button')
+      .contains('Logout')
+      .should('not.exist');
   });
 });
